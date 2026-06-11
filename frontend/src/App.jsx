33e7f947503643782254b6d777a6cf5c48aaab22
Joban009 <jobangamer678@gmail.com>
2026-06-11 20:13:45 +0545

    fix MainContainer reference error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       <Route
         element={
           <ProtectedRoute>
-            <Maincontainer />
+            <MainContainer />
           </ProtectedRoute>
         }
       >
