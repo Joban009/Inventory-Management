e4c07194ded6bacd0594e697bc8310667fe6d662
Joban009 <jobangamer678@gmail.com>
2026-06-11 19:51:29 +0545

    filename correction

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/pages/Login";
-import Maincontainer from "./components/pages/main/MainContainer";
+import MainContainer from "./components/pages/main/MainContainer";
 import Dashboard from "./components/pages/main/Dashboard";
 import Inventory from "./components/pages/main/Inventory";
 import Setting from "./components/pages/main/Setting";
