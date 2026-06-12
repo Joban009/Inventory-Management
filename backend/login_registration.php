a7203121708bf1b6295f37a3011d8c5a63613d59
Joban009 <jobangamer678@gmail.com>
2026-06-12 20:20:54 +0545

    cors path

--- backend/login_registration.php
+++ backend/login_registration.php
@@ -5,7 +5,7 @@ ini_set('session.cookie_secure', '1');
 ini_set('session.cookie_httponly', '1');
 
 // ✅ If cors.php is in backend/ root, path is ../cors.php
-require_once '../cors.php';
+require_once './cors.php';
 
 // ✅ content-type header after CORS
 header("Content-Type: application/json");
